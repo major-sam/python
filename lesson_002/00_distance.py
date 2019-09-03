6045bfcec2f3aa6f795efe33076127070a421e65
sam <veritas.sam@gmail.com>
2019-09-03 12:48:49 +0300

    lesson 2 part 00

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from pprint import pprint
 # Есть словарь координат городов
 
 sites = {
@@ -11,13 +11,22 @@ sites = {
 
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
-
 distances = {}
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+moscow_london = ((moscow[0]-london[0])**2 + (moscow[1]-london[1])**2)**.5
+moscow_paris = ((moscow[0]-paris[0])**2 + (moscow[1]-paris[1])**2)**.5
+london_paris = ((london[0]-paris[0])**2 + (london[1]-paris[1])**2)**.5
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = london_paris
 
-# TODO здесь заполнение словаря
-
-print(distances)
-
-
-
-
+pprint(distances)
+# Почему вывод начинается с Лондона ? сортировка по имени?
