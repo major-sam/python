12a9e8182eb492ededca190c8d9cbdbb145dd079
sam <veritas.sam@gmail.com>
2019-09-04 14:11:17 +0300

    lesson 3 part 00 circle staff

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -5,20 +5,25 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+def bubble(point, step, color):
+    radius = 50
+    for _ in range(3):
+        sd.circle(center_position=point, radius=radius, color=color, width=1)
+        radius -= step
+
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+for _ in range(100):
+    point = sd.random_point()
+    color = sd.random_color()
+    bubble(point, step=3, color=color)
 
 sd.pause()
-
-
