0a70a7e6d45209ff4b0b1084a3e1cb7890c9baf8
sam <veritas.sam@gmail.com>
2019-09-04 14:26:23 +0300

    lesson 3 part 01 get days in month

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -10,5 +10,12 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
-
-# TODO здесь ваш код
+if month == 2:
+    days_in_month = 28
+elif month in [1, 3, 5, 7, 8, 10, 12]:
+    days_in_month = 31
+elif month in [2, 4, 6, 9, 11]:
+    days_in_month = 30
+else:
+    print(month, '- неверный номер месяца')
+print('Кол-во дней в месяце', days_in_month)
