5f7f209d182d4eaa6b96306ef63743a3b58cfe9b
sam <veritas.sam@gmail.com>
2019-09-05 15:34:51 +0300

    l04 t02 чот я поторопился

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -14,86 +14,91 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-print('Возможные цвета'
-      '0 : red'
-      '1 : orange'
-      '2 : yellow'
-      '3 : green'
-      '4 : cyan'
-      '5 : blue'
-      '6 : purple')
+print('Возможные цвета\n'
+      '0 : red\n'
+      '1 : orange\n'
+      '2 : yellow\n'
+      '3 : green\n'
+      '4 : cyan\n'
+      '5 : blue\n'
+      '6 : purple\n')
 
 colors = {
-    0: sd.COLOR_RED,
-    1: sd.COLOR_ORANGE,
-    2: sd.COLOR_YELLOW,
-    3: sd.COLOR_GREEN,
-    4: sd.COLOR_CYAN,
-    5: sd.COLOR_BLUE,
-    6: sd.COLOR_PURPLE
+    '0': sd.COLOR_RED,
+    '1': sd.COLOR_ORANGE,
+    '2': sd.COLOR_YELLOW,
+    '3': sd.COLOR_GREEN,
+    '4': sd.COLOR_CYAN,
+    '5': sd.COLOR_BLUE,
+    '6': sd.COLOR_PURPLE
 }
 
-print('Выберете цвет фигур')
+user_input = input('Выберете цвет фигур\n')
+while user_input not in colors:
+    print('Вы ввели некоректный номер', user_input)
+    user_input = input('Выберете цвет фигур\n')
+user_color = colors[user_input]
 
 
 def triangle(triangle_start, angle, length, color):
-    side_a = sd.get_vector(start_point=triangle_start, angle=angle, length=length, color=color)
-    side_a.draw()
-    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 120, length=length, color=color)
-    side_b.draw()
-    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 240, length=length, color=color)
-    side_c.draw()
+    side_a = sd.get_vector(start_point=triangle_start, angle=angle, length=length)
+    side_a.draw(color=color)
+    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 120, length=length)
+    side_b.draw(color=color)
+    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 240, length=length)
+    side_c.draw(color=color)
 
 
 # - квадрата
 
 def quadro(quadro_start, angle, length, color):
-    side_a = sd.get_vector(start_point=quadro_start, angle=angle, length=length, color=color)
-    side_a.draw()
-    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 90, length=length, color=color)
-    side_b.draw()
-    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 180, length=length, color=color)
-    side_c.draw()
-    side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 270, length=length, color=color)
-    side_d.draw()
+    side_a = sd.get_vector(start_point=quadro_start, angle=angle, length=length)
+    side_a.draw(color=color)
+    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 90, length=length)
+    side_b.draw(color=color)
+    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 180, length=length)
+    side_c.draw(color=color)
+    side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 270, length=length)
+    side_d.draw(color=color)
 
 
 # - пятиугольника
 def pento(pento_start, angle, length, color):
-    side_a = sd.get_vector(start_point=pento_start, angle=angle, length=length, color=color)
-    side_a.draw()
-    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 72, length=length, color=color)
-    side_b.draw()
-    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 144, length=length, color=color)
-    side_c.draw()
-    side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 216, length=length, color=color)
-    side_d.draw()
-    side_e = sd.get_vector(start_point=side_d.end_point, angle=angle + 288, length=length, color=color)
-    side_e.draw()
+    side_a = sd.get_vector(start_point=pento_start, angle=angle, length=length)
+    side_a.draw(color=color)
+    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 72, length=length)
+    side_b.draw(color=color)
+    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 144, length=length)
+    side_c.draw(color=color)
+    side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 216, length=length)
+    side_d.draw(color=color)
+    side_e = sd.get_vector(start_point=side_d.end_point, angle=angle + 288, length=length)
+    side_e.draw(color=color)
 
 
 # - шестиугольника
 def hexagon(hexagon_start, angle, length, color):
-    side_a = sd.get_vector(start_point=hexagon_start, angle=angle, length=length, color=color)
-    side_a.draw()
-    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 60, length=length, color=color)
-    side_b.draw()
-    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 120, length=length, color=color)
-    side_c.draw()
-    side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 180, length=length, color=color)
-    side_d.draw()
-    side_e = sd.get_vector(start_point=side_d.end_point, angle=angle + 240, length=length, color=color)
-    side_e.draw()
-    side_f = sd.get_vector(start_point=side_e.end_point, angle=angle + 300, length=length, color=color)
-    side_f.draw()
+    side_a = sd.get_vector(start_point=hexagon_start, angle=angle, length=length)
+    side_a.draw(color=color)
+    side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 60, length=length)
+    side_b.draw(color=color)
+    side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 120, length=length)
+    side_c.draw(color=color)
+    side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 180, length=length)
+    side_d.draw(color=color)
+    side_e = sd.get_vector(start_point=side_d.end_point, angle=angle + 240, length=length)
+    side_e.draw(color=color)
+    side_f = sd.get_vector(start_point=side_e.end_point, angle=angle + 300, length=length)
+    side_f.draw(color=color)
 
 
 point_1 = sd.get_point(100, 100)
 point_2 = sd.get_point(100, 400)
 point_3 = sd.get_point(400, 100)
 point_4 = sd.get_point(400, 400)
-triangle(triangle_start=point_1, angle=321, length=100)
-quadro(quadro_start=point_2, angle=10, length=100)
-pento(pento_start=point_3, angle=22, length=100)
+triangle(triangle_start=point_1, angle=321, length=100, color=user_color)
+quadro(quadro_start=point_2, angle=10, length=100, color=user_color)
+pento(pento_start=point_3, angle=22, length=100, color=user_color)
+hexagon(hexagon_start=point_4, angle=22, length=100, color=user_color)
 
 sd.pause()
