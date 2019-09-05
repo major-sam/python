2d8e621ae25cfa0673ad46081297451c341b9d48
sam <veritas.sam@gmail.com>
2019-09-05 13:09:43 +0300

    l03 t08 add coordinates as external var

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -9,9 +9,9 @@ import simple_draw as sd
 # Вывести 10 смайликов в произвольных точках экрана.
 
 
-def smile(color):  # TODO в условиях это сказано немного противоречиво, но функция должна ещё принимать параметру X,Y
-    center_x = sd.random_number(100, 500)
-    center_y = sd.random_number(100, 500)
+def smile(random_center_x, random_center_y, color):
+    center_x = random_center_x
+    center_y = random_center_y
     center = sd.get_point(center_x, center_y)
     sd.circle(center, radius=60, color=color, width=2)
     mouth_y = center_y - 25
@@ -32,6 +32,8 @@ def smile(color):  # TODO в условиях это сказано немног
 
 
 for _ in range(10):
-    smile(sd.random_color())
+    center_a = sd.random_number(100, 500)
+    center_b = sd.random_number(100, 500)
+    smile(center_a, center_b, sd.random_color())
 sd.pause()
 # А в целом, отличная работа, в который раз! :)
