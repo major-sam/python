895e3713fb7e4ade3ea811e25879a05fd1768930
sam <veritas.sam@gmail.com>
2019-09-12 11:18:05 +0300

    lesson 4 part 03

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -3,75 +3,6 @@
 import simple_draw as sd
 
 
-# 
-# def triangle(triangle_start, angle, length, color):
-#     buffer = triangle_start
-#     side_a = sd.get_vector(start_point=triangle_start, angle=angle, length=length)
-#     side_a.draw(color=color)
-#     side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 120, length=length)
-#     side_b.draw(color=color)
-#     sd.line(buffer, side_b.end_point, color=color)
-# 
-# 
-# # - квадрата
-# def quadro(quadro_start, angle, length, color):
-#     buffer = quadro_start
-#     side_a = sd.get_vector(start_point=quadro_start, angle=angle, length=length)
-#     side_a.draw(color=color)
-#     side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 90, length=length)
-#     side_b.draw(color=color)
-#     side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 180, length=length)
-#     side_c.draw(color=color)
-#     sd.line(buffer, side_c.end_point, color=color)
-# 
-# 
-# # - пятиугольника
-# def pento(pento_start, angle, length, color):
-#     buffer = pento_start
-#     side_a = sd.get_vector(start_point=pento_start, angle=angle, length=length)
-#     side_a.draw(color=color)
-#     side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 72, length=length)
-#     side_b.draw(color=color)
-#     side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 144, length=length)
-#     side_c.draw(color=color)
-#     side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 216, length=length)
-#     side_d.draw(color=color)
-#     sd.line(buffer, side_d.end_point, color=color)
-# 
-# 
-# # - шестиугольника
-# def hexagon(hexagon_start, angle, length, color):
-#     buffer = hexagon_start
-#     side_a = sd.get_vector(start_point=hexagon_start, angle=angle, length=length)
-#     side_a.draw(color=color)
-#     side_b = sd.get_vector(start_point=side_a.end_point, angle=angle + 60, length=length)
-#     side_b.draw(color=color)
-#     side_c = sd.get_vector(start_point=side_b.end_point, angle=angle + 120, length=length)
-#     side_c.draw(color=color)
-#     side_d = sd.get_vector(start_point=side_c.end_point, angle=angle + 180, length=length)
-#     side_d.draw(color=color)
-#     side_e = sd.get_vector(start_point=side_d.end_point, angle=angle + 240, length=length)
-#     side_e.draw(color=color)
-#     sd.line(buffer, side_e.end_point, color=color)
-# 
-# 
-# def choosing_figure(figure_id, start_point, additional_angle=0, length=100, color=user_color):
-#     if figure_id == '0':
-#         triangle(start_point, additional_angle, length, color)
-#         sd.pause()
-#     elif figure_id == '1':
-#         quadro(start_point, additional_angle, length, color)
-#         sd.pause()
-#     elif figure_id == '2':
-#         pento(start_point, additional_angle, length, color)
-#         sd.pause()
-#     elif figure_id == '3':
-#         hexagon(start_point, additional_angle, length, color)
-#         sd.pause()
-#     else:
-#         print('wtf or something wrong with choosing_figure')
-
-
 def circle_vector(sides, start_point, additional_angle=0, length=100, color=sd.COLOR_DARK_YELLOW):
     if sides < 3:
         print('3+ sides needed for a figure')
@@ -143,15 +74,15 @@ print('Возможные фигуры\n')
 shapes = {
     '0':
         {'shape': 'треугольник',
-         'func': get_triangle},  # TODO хранить функции можно таким образом
+         'func': get_triangle},
     '1':
         {'shape': 'квадрат',
-         'func': 'get_quadro(midpoint, additional_angle=0, length=100, color=user_color)'},
+         'func': get_quadro},
     '2':
         {'shape': 'пятиугольник',
-         'func': 'get_pento(midpoint, additional_angle=0, length=100, color=user_color)'},
+         'func': get_pento},
     '3': {'shape': 'шестиугольник',
-          'func': 'get_hex(midpoint, additional_angle=0, length=100, color=user_color)'}
+          'func': get_hex}
 }
 for shape_id, shape_name in shapes.items():
     print(shape_id, ':', shape_name['shape'])
@@ -160,9 +91,7 @@ user_shape = input('Выберете фигуру\n')
 while user_shape not in shapes.keys():
     print('Вы ввели некоректный номер', user_shape)
     user_shape = input('Выберете фигуру\n')
-exec(shapes[user_shape]['func'])  # TODO затем эту функцию с нужными параметрами вызываем здесь
-# TODO что-то вроде функция = словарь[адрес]
-# TODO функция(параметры)
-sd.pause()
+func = shapes[user_shape]['func']
+func(midpoint, additional_angle=0, length=100, color=user_color)
 
-# TODO и нужно подчистить лишний код сверху перед зачетом.
+sd.pause()
